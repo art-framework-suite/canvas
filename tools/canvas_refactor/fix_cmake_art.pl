31dd69d86b4f94221d8d06bbc4aabbd3e7100c46
Kyle Knoepfel <knoepfel@fnal.gov>
2016-02-19 11:37:46 -0600

    Fix typo

--- tools/canvas_refactor/fix_cmake_art.pl
+++ tools/canvas_refactor/fix_cmake_art.pl
@@ -2,18 +2,18 @@ use strict;
 
 use vars qw(%dir_list);
 BEGIN { %dir_list = (
-	"art_Persistncy_Common" => "art_Persistncy_Common canvas_Persistency_Common",
-	"art_Persistency_Provenance" => "art_Persistency_Provenance canvas_Persistency_Provenance",
-	"art_Persistency_Common_dict" => "canvas_Persistency_Common_dict",
-	"art_Persistency_StdDictionaries" => "canvas_Persistency_StdDictionaries",
-	"art_Persistency_StdDictionaries_dict" => "canvas_Persistency_StdDictionaries_dict",
-	"art_Persistency_WrappedStdDictionaries" => "canvas_Persistency_WrappedStdDictionaries",
-	"art_Persistency_WrappedStdDictionaries_dict" => "canvas_Persistency_WrappedStdDictionaries_dict",
-	"art_Utilities" => "art_Utilities canvas_Utilities"
-		       ); }
+        "art_Persistency_Common" => "art_Persistency_Common canvas_Persistency_Common",
+        "art_Persistency_Provenance" => "art_Persistency_Provenance canvas_Persistency_Provenance",
+        "art_Persistency_Common_dict" => "canvas_Persistency_Common_dict",
+        "art_Persistency_StdDictionaries" => "canvas_Persistency_StdDictionaries",
+        "art_Persistency_StdDictionaries_dict" => "canvas_Persistency_StdDictionaries_dict",
+        "art_Persistency_WrappedStdDictionaries" => "canvas_Persistency_WrappedStdDictionaries",
+        "art_Persistency_WrappedStdDictionaries_dict" => "canvas_Persistency_WrappedStdDictionaries_dict",
+        "art_Utilities" => "art_Utilities canvas_Utilities"
+                       ); }
 
 foreach my $lib (sort keys %dir_list) {
-   next if m&art_Persistncy_Common canvas_Persistency_Common&i;
+   next if m&art_Persistency_Common canvas_Persistency_Common&i;
    next if m&art_Persistency_Provenance canvas_Persistency_Provenance&i;
    next if m&art_Utilities canvas_Utilities&i;
   #s&\b\Q${lib}\E([^\.\s]*\b)([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
