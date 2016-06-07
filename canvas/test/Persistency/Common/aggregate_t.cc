195134ae7d1853e9851bc4b60df9364b518cbfe1
Chris Green <artists@fnal.gov>
2016-06-07 17:01:01 -0500

    Remove redundant header.

--- canvas/test/Persistency/Common/aggregate_t.cc
+++ canvas/test/Persistency/Common/aggregate_t.cc
@@ -5,7 +5,6 @@
 // N.B. art::PtrVector cannot be overloaded
 
 #include "cetlib/quiet_unit_test.hpp"
-#include "boost/test/test_tools.hpp"
 #include "canvas/test/Persistency/Common/MockRun.h"
 #include "cetlib/map_vector.h"
 #include "cetlib/test_macros.h"
