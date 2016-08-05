1712daf17402363915c497dd16901b64836816b1
Kyle Knoepfel <knoepfel@fnal.gov>
2016-08-05 09:19:02 -0500

    Metaprogramming improvements.

--- canvas/Persistency/Common/traits.cc
+++ canvas/Persistency/Common/traits.cc
@@ -1,7 +1,4 @@
-
 #include "canvas/Persistency/Common/traits.h"
 
-
-const art::key_traits<std::string>::key_type
-art::key_traits<std::string>::value = std::string("\a");
-
+art::key_traits<std::string>::key_type const
+art::key_traits<std::string>::value {std::string("\a")};
