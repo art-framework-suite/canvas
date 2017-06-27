2901a14e88f53c88c39d35c04f14514caacb8add
Kyle Knoepfel <knoepfel@fnal.gov>
2017-06-27 13:35:14 -0500

    Change HLTGlobalStatus *interface* to use std::size_t instead of unsigned.
    Does not change schema of HLTGlobalStatus.

--- canvas/Persistency/Common/HLTGlobalStatus.h
+++ canvas/Persistency/Common/HLTGlobalStatus.h
@@ -3,22 +3,18 @@
 
 /** \class art::HLTGlobalStatus
  *
- *
  *  The HLT global status, summarising the status of the individual
  *  HLT triggers, is implemented as a vector of HLTPathStatus objects.
  *
  *  If the user wants map-like indexing of HLT triggers through their
  *  names as key, s/he must use the TriggerNamesService.
  *
- *
- *
- *
  *  \author Martin Grunewald
- *
  */
 
 #include "canvas/Persistency/Common/HLTenums.h"
 #include "canvas/Persistency/Common/HLTPathStatus.h"
+#include "cetlib/container_algorithms.h"
 #include <vector>
 #include <ostream>
 
@@ -27,54 +23,54 @@
 namespace art {
 
   class HLTGlobalStatus {
-  private:
     // Status of each HLT path
-    std::vector<HLTPathStatus> paths_;
+    std::vector<HLTPathStatus> paths_{};
 
   public:
 
     // Constructor - for n paths
-    explicit HLTGlobalStatus(const unsigned int n=0) : paths_(n) {}
+    HLTGlobalStatus() = default;
+    explicit HLTGlobalStatus(std::size_t const n) : paths_(n) {}
 
     // Get number of paths stored
-    unsigned int size() const { return paths_.size(); }
+    std::size_t size() const { return paths_.size(); }
 
     // Reset status for all paths
     void reset() {
-      const unsigned int n(size());
-      for (unsigned int i = 0; i != n; ++i) paths_[i].reset();
+      cet::for_all(paths_, [](auto& path){ path.reset(); });
     }
 
     // global "state" variables calculated on the fly!
 
     // Was at least one path run?
-    bool wasrun() const {return State(0);}
+    bool wasrun() const {return state_on_demand(0);}
     // Has at least one path accepted the event? If no paths were
     // run, or there are no paths, the answer is, "yes."
-    bool accept() const {return State(1);}
+    bool accept() const {return state_on_demand(1);}
     // Has any path encountered an error (exception)
-    bool  error() const {return State(2);}
+    bool  error() const {return state_on_demand(2);}
 
     // get hold of individual elements, using safe indexing with "at" which throws!
+    HLTPathStatus const& at (std::size_t const i) const { return paths_.at(i); }
+    HLTPathStatus& at (std::size_t const i) { return paths_.at(i); }
 
-    const HLTPathStatus& at (const unsigned int i)   const { return paths_.at(i); }
-    HLTPathStatus& at (const unsigned int i)         { return paths_.at(i); }
-    const HLTPathStatus& operator[](const unsigned int i) const { return paths_.at(i); }
-    HLTPathStatus& operator[](const unsigned int i)       { return paths_.at(i); }
+    HLTPathStatus const& operator[](std::size_t const i) const { return paths_.at(i); }
+    HLTPathStatus& operator[](std::size_t const i) { return paths_.at(i); }
+
 
     // Was ith path run?
-    bool wasrun(const unsigned int i) const { return at(i).wasrun(); }
+    bool wasrun(std::size_t const i) const { return at(i).wasrun(); }
     // Has ith path accepted the event?
-    bool accept(const unsigned int i) const { return at(i).accept(); }
+    bool accept(std::size_t const i) const { return at(i).accept(); }
     // Has ith path encountered an error (exception)?
-    bool  error(const unsigned int i) const { return at(i).error() ; }
+    bool  error(std::size_t const i) const { return at(i).error() ; }
 
     // Get status of ith path
-    hlt::HLTState state(const unsigned int i) const { return at(i).state(); }
+    hlt::HLTState state(std::size_t const i) const { return at(i).state(); }
     // Get index (slot position) of module giving the decision of the ith path
-    unsigned int  index(const unsigned int i) const { return at(i).index(); }
+    std::size_t  index(std::size_t const i) const { return at(i).index(); }
     // Reset the ith path
-    void reset(const unsigned int i) { at(i).reset(); }
+    void reset(std::size_t const i) { at(i).reset(); }
     // swap function
     void swap(HLTGlobalStatus& other) { paths_.swap(other.paths_); }
     // copy assignment implemented with swap()
@@ -86,22 +82,24 @@ namespace art {
     }
 
   private:
+
     // Global state variable calculated on the fly
-    bool State(unsigned int icase) const {
-      bool flags[3] = {false, false, false};
-      const unsigned int n(size());
-      for (unsigned int i = 0; i != n; ++i) {
-        const hlt::HLTState s(state(i));
+    bool state_on_demand(std::size_t const icase) const
+    {
+      bool flags[3] {false, false, false};
+      for (std::size_t i{}, n{size()}; i != n; ++i) {
+        auto const s = state(i);
         if (s!=hlt::Ready) {
-          flags[0]=true;       // at least one trigger was run
+          flags[0]=true;    // at least one trigger was run
           if (s==hlt::Pass) {
-            flags[1]=true;     // at least one trigger accepted
-          } else if (s==hlt::Exception) {
-            flags[2]=true;     // at least one trigger with error
+            flags[1]=true;  // at least one trigger accepted
+          }
+          else if (s==hlt::Exception) {
+            flags[2]=true;  // at least one trigger with error
           }
         }
       }
-      // Change in semantics of flags[1] vs pre-ART: now we accept if
+      // Change in semantics of flags[1] vs pre-art: now we accept if
       // no paths were run.
       flags[1] |= (!flags[0]) | paths_.empty();
       return flags[icase];
@@ -116,13 +114,12 @@ namespace art {
     lhs.swap(rhs);
   }
 
-  // Formatted printout of trigger tbale
+  // Formatted printout of trigger table
   inline std::ostream&
-  operator <<(std::ostream& ost, const HLTGlobalStatus& hlt)
+  operator<<(std::ostream& ost, HLTGlobalStatus const& hlt)
   {
     std::vector<std::string> text(4); text[0]="n"; text[1]="1"; text[2]="0"; text[3]="e";
-    const unsigned int n(hlt.size());
-    for (unsigned int i = 0; i != n; ++i) ost << text.at(hlt.state(i));
+    for (std::size_t i{}, n{hlt.size()}; i != n; ++i) ost << text.at(hlt.state(i));
     return ost;
   }
 
