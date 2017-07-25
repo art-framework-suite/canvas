4a3598aa1741e6268e9d0c2e02cf572e25e8cce9
Kyle Knoepfel <knoepfel@fnal.gov>
2017-07-25 13:34:13 -0500

    Accept ProductToken from art.

---
+++ canvas/Persistency/Common/ProductToken.h
@@ -0,0 +1,40 @@
+#ifndef canvas_Persistency_Common_ProductToken_h
+#define canvas_Persistency_Common_ProductToken_h
+
+//==============================================================
+// ProductToken is used to enable efficient product lookup via a
+// consumes statement given in a module's constructor (e.g.):
+//
+//   ProductToken<int> nPotsToken_{consumes<int>{inputTag_}};
+//   ...
+//   auto const& nPotsH = e.getByToken(nPotsToken_); => Handle<int>;
+//
+// It is an all-private class that only ConsumesRecorder and
+// DataViewImpl can access.
+// ==============================================================
+
+#include "canvas/Utilities/InputTag.h"
+
+namespace art {
+
+  class ConsumesRecorder;
+  class DataViewImpl;
+
+  template <typename T>
+  class ProductToken {
+    friend class ConsumesRecorder;
+    friend class DataViewImpl;
+
+    auto const& inputTag() const { return inputTag_; }
+    explicit ProductToken() = default;
+    explicit ProductToken(InputTag const& t) : inputTag_{t} {}
+    InputTag inputTag_{};
+  };
+
+}
+
+#endif /* canvas_Persistency_Common_ProductToken_h */
+
+// Local Variables:
+// mode: c++
+// End:
