ed37317d1491c5d1102c9777890d9660595c03a5
Marc Paterno <paterno@fnal.gov>
2018-01-17 14:05:27 -0600

    Remove unused test files

--- canvas/test/Persistency/Common/TestHandle.h
+++
@@ -1,8 +0,0 @@
-#ifndef canvas_test_Persistency_Common_TestHandle_h
-#define canvas_test_Persistency_Common_TestHandle_h
-#include "art/Persistency/Common/TestHandle.h"
-#endif /* canvas_test_Persistency_Common_TestHandle_h */
-
-// Local Variables:
-// mode: c++
-// End:
