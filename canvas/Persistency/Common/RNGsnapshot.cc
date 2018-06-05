2829a88b704dda9f539aff61efd13297f05ef965
Kyle Knoepfel <knoepfel@fnal.gov>
2018-06-05 09:27:46 -0500

    Update comments.

--- canvas/Persistency/Common/RNGsnapshot.cc
+++ canvas/Persistency/Common/RNGsnapshot.cc
@@ -24,6 +24,7 @@ namespace art {
   {
     engine_kind_ = ekind;
     label_ = lbl;
+    // N.B. implicit conversion from unsigned long to unsigned int.
     cet::copy_all(est, std::back_inserter(state_));
   }
 
@@ -31,6 +32,7 @@ namespace art {
   RNGsnapshot::restoreState() const
   {
     std::vector<unsigned long> est;
+    // N.B. implicit conversion from unsigned long to unsigned int.
     cet::copy_all(state_, std::back_inserter(est));
     return est;
   }
