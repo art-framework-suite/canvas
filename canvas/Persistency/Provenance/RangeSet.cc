b009fe668f20d751a352117cc51fcd13cf1016f8
Kyle Knoepfel <knoepfel@fnal.gov>
2024-07-03 14:34:08 -0500

    Explicitly discard return value of std::unique

--- canvas/Persistency/Provenance/RangeSet.cc
+++ canvas/Persistency/Provenance/RangeSet.cc
@@ -306,7 +306,7 @@ namespace art {
                other.ranges_.cbegin(),
                other.ranges_.cend(),
                back_inserter(merged));
-    unique(merged.begin(), merged.end());
+    (void)unique(merged.begin(), merged.end()); // discarding return value
     std::swap(ranges_, merged);
     isCollapsed_ = false;
     collapse();
